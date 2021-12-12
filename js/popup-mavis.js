8204e4681a90a6771e1a8d475919ef6c18b19bf3
uchenma <uchenma.97@gmail.com>
2021-12-12 02:01:20 -0500

    added icon and jquery library

--- js/popup-mavis.js
+++ js/popup-mavis.js
@@ -1,1 +1,8 @@
+$(document).ready(function () {
 
+    $("#finish-button").click(function () {
+        $("#set-up-view").addClass("hidden");
+        $("#authentication-view").removeClass("hidden");
+    })
+
+});
