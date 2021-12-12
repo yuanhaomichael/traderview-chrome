703bd6fbd11540f1f8399d1d17777b8b4fbb8fc9
Michael Yuanhao Deng <54728835+yuanhaomichael@users.noreply.github.com>
2021-12-12 11:51:31 -0500

    alert dashboard completed

---
+++ alert.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+
+    $("#alert-tesla").click(function () {
+        console.log("hi")
+        $("#alerts-dashboard").addClass("hidden");
+        $("#alerts-page").removeClass("hidden");
+    })
+
+    $("#alert-apple").click(function () {
+        console.log("hi")
+        $("#alerts-dashboard").addClass("hidden");
+        $("#alerts-page").removeClass("hidden");
+    })
+
+    $("#alert-alphabet").click(function () {
+        console.log("hi")
+        $("#alerts-dashboard").addClass("hidden");
+        $("#alerts-page").removeClass("hidden");
+    })
+
+    $("#alert-walmart").click(function () {
+        console.log("hi")
+        $("#alerts-dashboard").addClass("hidden");
+        $("#alerts-page").removeClass("hidden");
+    })
+
+});
